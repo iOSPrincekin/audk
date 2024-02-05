150771c96120b88def0ea9ee2b81556e68fc4f48
Mikhail Krichanov <mikhailkrichanov@gmail.com>
2024-02-05 17:26:13 +0300

    Ring3: Added SysCallInstallMultipleProtocolInterfaces draft.

--- MdePkg/Include/Uefi/UefiSpec.h
+++ MdePkg/Include/Uefi/UefiSpec.h
@@ -1963,8 +1963,9 @@ typedef struct {
 } EFI_BOOT_SERVICES;
 
 typedef enum {
-  SysCallLocateProtocol     = 1,
-  SysCallOpenProtocol       = 2,
+  SysCallLocateProtocol                    = 1,
+  SysCallOpenProtocol                      = 2,
+  SysCallInstallMultipleProtocolInterfaces = 3,
   SysCallMax
 } SYS_CALL_TYPE;
 
